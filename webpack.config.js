0751a548d83e4ab93031831a4b3a875de11fa582
subishga06 <subishgavs@gmail.com>
2026-01-05 19:04:52 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
 const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
-module.exports = {
-    entry: './src/index.js',
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+module.exports = {
+  entry: "./src/index.js", // Main entry point
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    clean: true, // Clears old files in dist on build
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"], // So we can import without extensions
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Your HTML template
+    }),
+  ],
+
+  devServer: {
+    static: path.join(__dirname, "dist"), // Serve dist folder
+    port: 8080, // Cypress expects 8080
+    hot: true, // Enables hot reload
+    open: true, // Opens browser automatically
+  },
+
+  mode: "development",
 };
